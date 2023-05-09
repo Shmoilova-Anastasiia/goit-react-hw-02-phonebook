bf147d05b7d3f4df3f2754a6d0a6d1e813330518
Shmoilova-Anastasiia <114925791+Shmoilova-Anastasiia@users.noreply.github.com>
2023-05-10 00:04:48 +0300

    add code

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.div`
+  padding: 15px 30px;
+ margin: auto;
+`;
+
+export const Section = styled.section`
+ margin: 0 auto;
+  &:not(:last-child) {
+    margin-bottom: 15px;
+  }
+`;
+
+export const TitleH1 = styled.h1`
+  margin-bottom: 15px;
+  font-size: 24px;
+  font-weight: bold;
+`;
+ 
+
+export const TitleH2 = styled.h2`
+  margin-bottom: 15px;
+  font-size: 20px;
+  font-weight: bold; 
+`;
